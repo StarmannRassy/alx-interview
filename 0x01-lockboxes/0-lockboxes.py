ec7db32601e0a254b3ab746ba5374065b98537cb
StarmannRassy <boronicle@gmail.com>
2023-07-04 13:05:00 +0000

    lockboxes sollution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,31 @@
-def canUnlockAll(boxes):
-    n = len(boxes)  # Total number of boxes
-    unlocked = [False] * n  # Initialize a list to track unlocked boxes
-    unlocked[0] = True  # The first box is unlocked by default
+'''Lockboxes Challenge'''
 
-    stack = [0]  # Start with the first box in the stack
+def canUnlockAll(boxes):
+    '''
+    Determines if all the boxes can be opened or not
+    Returns:
+    	True: if all the boxes can be opened
+    	False: if all the boxes cant be opened
+    '''
+    # Total number of boxes
+    n = len(boxes)  
+    # Initialize a list to track unlocked boxes
+    unlocked = [False] * n
+    # The first box is unlocked by default  
+    unlocked[0] = True  
+
+    # Start with the first box in the stack
+    stack = [0]  
     while stack:
-        box = stack.pop()  # Take a box from the stack
-        for key in boxes[box]:  # Iterate over the keys in the box
+    	# Take a box from the stack
+        box = stack.pop()  
+        # Iterate over the keys in the box
+        for key in boxes[box]:  
+            # Check if the key is valid and the box is locked
             if key < n and not unlocked[key]: 
-                # Check if the key is valid and the box is locked
-                unlocked[key] = True  # Unlock the box
-                stack.append(key)  # Add the newly unlocked box to the stack
+                unlocked[key] = True  
+                stack.append(key) 
+    # Return True if all boxes are unlocked, else return False 
     return all(unlocked) 
-# Return True if all boxes are unlocked, else return False
+
 
